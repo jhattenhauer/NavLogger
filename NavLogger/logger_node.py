51ca97b16e6815e2d9c06478b3f97fb7e5aee083
jimmy <jhattenhauer@outlook.com>
2025-02-15 08:12:19 -0330

    Added file header creation, detects if file in already populated

--- NavLogger/logger_node.py
+++ NavLogger/logger_node.py
@@ -3,6 +3,10 @@ from rclpy.node import Node
 from sensor_msgs.msg import NavSatFix
 from mavros_msgs.msg import State
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
+import os
+
+file_path = '/home/james/Documents/dev/NavLogger/test.waypoints'
+file = open(file_path, 'a')
 
 class logger(Node):
     def __init__(self):
@@ -29,16 +33,19 @@ class logger(Node):
         )
 
     def listener_callback(self, global_msg, state_msg):
-        if str(state_msg.mode) == "GUIDED":   
-            file = open("/home/james/Documents/dev/NavLogger/test.waypoints", 'r')
-            index = str(file.readlines()[-1])[0]
-            message = f"{index+1}\t0\t3\t16\t0.00000000\t0.00000000\t0.00000000\t0.00000000\t{global_msg.latitude}\t{global_msg.longitude}\t{global_msg.altitude}\t100.000000"
-            file = open("/home/james/Documents/dev/NavLogger/test.waypoints", 'a')
-            file.write(message)
 
+        if str(state_msg.mode) == "GUIDED":   
+            try:        
+                node_number = str(file.readlines()[-1])[0]
+                message = f"{node_number+1}\t0\t3\t16\t0.00000000\t0.00000000\t0.00000000\t0.00000000\t{global_msg.latitude}\t{global_msg.longitude}\t{global_msg.altitude}\t100.000000"            
+                file.write(message)
+            except:
+                print("An Error Occured")
 #example   6	0	3	16	0.00000000	0.00000000	0.00000000	0.00000000	47.57154050	-52.82818790	100.000000	1
 
 def main(args=None):
+    if os.stat(file_path).st_size == 0: #checks and writes if header line in file
+        file.write('QGC WPL 110\n0\t1\t0\t0\t0\t0\t0\t0\t0\t0\t0\t1')
     rclpy.init(args=args)
     node = logger()
     rclpy.spin(node)
